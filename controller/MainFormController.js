ba6d761733f1e65aa0103dbb16152173307d2beb
ShanPereraX <perera.alc2000@gmail.com>
2023-07-04 22:46:38 +0530

    navigation : done

---
+++ controller/MainFormController.js
@@ -0,0 +1,15 @@
+function navigate(id) {
+    $('#manage_customer').css('display', id == 'manage_customer' ? 'flex' : 'none');
+    $('#manage_item').css('display', id == 'manage_item' ? 'flex' : 'none');
+    $('#menu-section').css('display', id == 'home' ? 'flex' : 'none');
+}
+document.getElementById('customer').addEventListener('click',
+    (e) => {
+    e.preventDefault()
+    navigate('manage_customer');
+});
+document.getElementById('item').addEventListener('click',
+    (e) => {
+    e.preventDefault()
+    navigate('manage_item');
+});
